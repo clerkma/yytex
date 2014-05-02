8ec2dfdd262d64a183af014b7f4e0d82f6456c33
clerkma@gmail.com <clerkma@gmail.com@cbf3535c-292a-0410-b4d2-4bbcb1e8a44f>
2014-05-02 18:59:58 +0000

    imported kpathsea.

---
+++ src/texsourc/kpathsea/kpathsea/line.c
@@ -0,0 +1,72 @@
+/* line.c: return the next line from a file, or NULL.
+
+   Copyright 1992, 1993, 1995, 1996, 2008, 2013 Karl Berry.
+   Copyright 1998, 1999, 2001, 2005 Olaf Weber.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this library; if not, see <http://www.gnu.org/licenses/>.  */
+
+#include <kpathsea/config.h>
+#include <kpathsea/line.h>
+#ifdef WIN32
+#undef getc
+#undef ungetc
+#define getc   win32_getc
+#define ungetc win32_ungetc
+#endif
+
+/* Allocate in increments of this size.  */
+#define BLOCK_SIZE 75
+
+char *
+read_line (FILE *f)
+{
+  int c;
+  unsigned limit = BLOCK_SIZE;
+  unsigned loc = 0;
+  char *line = xmalloc (limit);
+
+  while ((c = getc (f)) != EOF && c != '\n' && c != '\r') {
+    line[loc] = c;
+    loc++;
+
+    /* By testing after the assignment, we guarantee that we'll always
+       have space for the null we append below.  We know we always
+       have room for the first char, since we start with BLOCK_SIZE.  */
+    if (loc == limit) {
+      limit += BLOCK_SIZE;
+      line = xrealloc (line, limit);
+    }
+  }
+
+  /* If we read anything, return it, even a partial last-line-if-file
+     which is not properly terminated.  */
+  if (loc == 0 && c == EOF) {
+    /* At end of file.  */
+    free (line);
+    line = NULL;
+  } else { 
+    /* Terminate the string.  We can't represent nulls in the file,
+       but this doesn't matter.  */
+    line[loc] = 0;
+    /* Absorb LF of a CRLF pair. */
+    if (c == '\r') {
+      c = getc (f);
+      if (c != '\n') {
+        ungetc (c, f);
+      }
+    }
+  }
+
+  return line;
+}
